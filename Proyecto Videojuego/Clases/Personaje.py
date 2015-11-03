bb68b9b8be7b0c1615dd2ee21eda434935e59419
maximilianoPizarro <maximiliano.pizarro.5@gmail.com>
2015-11-03 11:21:19 -0300

    se encuentra en modelo

--- Proyecto Videojuego/Clases/Personaje.py
+++
@@ -1,6 +0,0 @@
-#Esta la estructura de basica de una clase en python.
-class Personaje(object):                                             #El parametro object se pone para poder crear subclases apartir de esta clase.
-    def __init__(self):                                          #La funcion __ini__ se ejecuta cuando se crea la clase.
-        self.imagen = 'Este es atributo de la clase'           #El parametro self no se que significa pero tiene que ir.
-        self.x
-        self.y
