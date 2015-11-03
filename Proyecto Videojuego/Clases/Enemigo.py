63f0cbd1430dcb2751d4c21dd3ac2eecb0bdeb98
maximilianoPizarro <maximiliano.pizarro.5@gmail.com>
2015-11-03 11:20:20 -0300

    se encuentra en modelo

--- Proyecto Videojuego/Clases/Enemigo.py
+++
@@ -1,8 +0,0 @@
-import Personaje.py
-
-
-class Enemigo(Personaje):                                           #Esta es la SubClase de Clase. Se define igual pero poniendo como parametro la clase base.
-    def __init__(self):                                          #La funcion __init__ aca esta puesta para llamar a __init__ de la clase base
-        super(Enemigo,self).__init__                           #A mi me funciona sin tener que ponerla pero asi compila seguro.
-    def desplazarIzquierda(self):
-        print 'en contruccion'
