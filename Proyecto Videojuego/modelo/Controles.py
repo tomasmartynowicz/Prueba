64eb6507f23c451e0cbbad90e704e433521e2276
maximilianoPizarro <maximiliano.pizarro.5@gmail.com>
2015-11-17 03:01:03 -0300

    Delete Controles.py

--- Proyecto Videojuego/modelo/Controles.py
+++
@@ -1,8 +0,0 @@
-class Controles (object):
-    instance = None
-    def __init__(self):                                          #La funcion __ini__ se ejecuta cuando se crea la clase.
-        self.ArrayControles
-    def __new__(cls, *args, **kargs):
-        if cls.instance is None:
-            cls.instance = object.__new__(cls, *args, **kargs)
-        return cls.instance
