37934a9018293629314702e0f0bc9652d4770d34
maximilianoPizarro <maximiliano.pizaro.5@gmail.com>
2015-11-17 02:27:03 -0300

    cambiando Modelo por modelo

--- modelo/Personaje.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-class Personaje():                                             #El parametro object se pone para poder crear subclases apartir de esta clase.
-    def __init__(self,imagen,x,y):                                          #La funcion __ini__ se ejecuta cuando se crea la clase.
-        self.imagen = pygame.image.load(imagen).convert_alpha()           #El parametro self no se que significa pero tiene que ir.
-        self.x=x
-        self.y=y
