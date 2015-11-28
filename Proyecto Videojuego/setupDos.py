85ae53d58c2819c824f54bdbae05e953d01b33f0
maximilianoPizarro <maximiliano.pizaro.5@gmail.com>
2015-11-28 13:12:42 -0300

    Instalacion:con icono y app sin fondo win 32

--- Proyecto Videojuego/setupDos.py
+++ Proyecto Videojuego/setupDos.py
@@ -1,11 +1,25 @@
 import cx_Freeze
 
-executables=[cx_Freeze.Executable("Jump The Rock.py")]
+import sys
+
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+executables=[cx_Freeze.Executable("Jump The Rock.py",icon="Perro.ico",base=base)]
+
+
 
 cx_Freeze.setup(
-    name="Jump the Rock",
-     options={"build_exe":{"packages":["pygame"],"include_files":["image","sound","font"]}},
-    description="Jump The Rock - videojuego",
-    executables=executables
+    
+	name="Jump the Rock",
+     
+	options={"build_exe":{"packages":["pygame"],"include_files":["image","sound","font"]}},
+    
+	description="Jump The Rock - videojuego",
+    	
+	executables=executables
 
-    )
+    
+	)
